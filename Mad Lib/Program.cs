f09d81a2e44ab9a0b310d3395db241d04361fe54
Thimosz <77938296+Thimosz@users.noreply.github.com>
2022-04-20 20:50:11 +0200

    20-04 money maker

---
+++ Mad Lib/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace MadLibs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            This program Maakt een klein verhaal met input van de gebruiker
+            Author: Thimo Spijker
+            */
+
+
+            // Let the user know that the program is starting:
+            Console.WriteLine("Laten we beginnen een verhaal te maken.");
+
+            // Give the Mad Lib a title:
+            string title = "Een dag uit het levern van:";
+
+            Console.WriteLine(title);
+            // Define user input and variables:
+            Console.Write("Enter a Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter a mood: ");
+            string mood = Console.ReadLine();
+            Console.Write("Enter a favorite color: ");
+            string favColor = Console.ReadLine();
+            Console.Write("Enter a favorite texture: ");
+            string favTexture = Console.ReadLine();
+            Console.Write("Enter a action like sit, sleep, eat, etc: ");
+            string action = Console.ReadLine();
+            Console.Write("Enter a person like girl or boy: ");
+            string person = Console.ReadLine();
+            Console.Write("Enter a place: ");
+            string place = Console.ReadLine();
+            Console.Write("Enter a Animal: ");
+            string animal = Console.ReadLine();
+            Console.Write("Entar a food: ");
+            string food = Console.ReadLine();
+            Console.Write("Enter a Fruit: ");
+            string fruit = Console.ReadLine();
+            Console.Write("Enter a superhero: ");
+            string superHero = Console.ReadLine();
+            Console.Write("Enter a Country: ");
+            string country = Console.ReadLine();
+            Console.Write("Enter a dessert: ");
+            string dessert = Console.ReadLine();
+            Console.Write("Enter a year: ");
+            string year = Console.ReadLine();
+
+
+            // The template for the story:
+
+            string story = $"This morning {name} woke up feeling {mood}. 'It is going to be a {favColor} day!' Outside, a bunch of {animal}s were protesting to keep {food} in stores. They began to {action} to the rhythm of the {person}, which made all the {fruit}s very {favTexture}. Concerned, {name} texted {superHero}, who flew {name} to {country} and dropped {name} in a puddle of frozen {dessert}. {name} woke up in the year {year}, in a world where {place}s ruled the world.";
+
+
+            // Print the story:
+            Console.WriteLine(story);
+        }
+    }
+}
---
+++ Money Maker/Money Maker/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Money_Maker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Money Maker!");
+            
+            Console.WriteLine("Wich amount of cents would you like to convert to coins?: ");
+            string amount = Console.ReadLine();
+            double amountDouble = Math.Floor(Convert.ToDouble(amount));
+            Console.WriteLine(" ");
+            Console.WriteLine($"{amountDouble} cents is equal to...");
+            Console.WriteLine(" ");
+
+            int gold = 10;
+            int silver = 5;
+
+            double goldCoins = Math.Floor(amountDouble / gold);
+            double leftOver = amountDouble % gold;
+
+            double silverCoins = Math.Floor(leftOver / silver);
+            leftOver = leftOver % silver;           
+          
+            Console.WriteLine($"Gold coins: {goldCoins}");
+            Console.WriteLine($"Silver coins: {silverCoins}");
+            Console.WriteLine($"Bronze coins: {leftOver}");
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Press any key to close the program...");
+            Console.ReadKey();
+            
+        }
+    }
+}
